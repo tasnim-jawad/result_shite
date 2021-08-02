6c789107fd6b74025caec4e737a23b66203b2692
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2021-08-02 06:00:00 +0600

    result

---
+++ js/result.js
@@ -0,0 +1,222 @@
+
+function gread_bangla(number){
+
+    if(number <0 || number > 100){
+
+        document.getElementById("result_show_bangla").innerHTML ='input valide number'.toUpperCase();
+        alert('Please Check Your Bangla subject Number')
+     
+    }else if(number >=33 && number < 40){
+
+        document.getElementById("result_show_bangla").innerHTML ='bangla: d'.toUpperCase();
+
+    }else if(number >=40 && number < 50){
+
+        document.getElementById("result_show_bangla").innerHTML ='bangla: c'.toUpperCase();
+
+    }else if(number >=50 && number < 60){
+
+        document.getElementById("result_show_bangla").innerHTML ='bangla: b'.toUpperCase();
+
+    }else if(number >=60 && number < 70){
+
+        document.getElementById("result_show_bangla").innerHTML ='bangla: a-'.toUpperCase();
+
+    }else if(number >=70 && number < 80){
+
+        document.getElementById("result_show_bangla").innerHTML ='bangla: a'.toUpperCase();
+
+    }else if(number >=80 && number < 100){
+
+        document.getElementById("result_show_bangla").innerHTML ='bangla: a+'.toUpperCase();
+    }else{
+
+        document.getElementById("result_show_bangla").innerHTML ='you are fail in bangla'.toUpperCase();
+
+    }
+}
+
+function gread_english(number){
+
+    if(number <0 || number > 100){
+
+        document.getElementById("result_show_english").innerHTML ='input valide number'.toUpperCase();
+        alert('Please Check Your english subject Number')
+     
+    }else if(number >=33 && number < 40){
+
+        document.getElementById("result_show_english").innerHTML ='english: d'.toUpperCase();
+
+    }else if(number >=40 && number < 50){
+
+        document.getElementById("result_show_english").innerHTML ='english: c'.toUpperCase();
+
+    }else if(number >=50 && number < 60){
+
+        document.getElementById("result_show_english").innerHTML ='english: b'.toUpperCase();
+
+    }else if(number >=60 && number < 70){
+
+        document.getElementById("result_show_english").innerHTML ='english: a-'.toUpperCase();
+
+    }else if(number >=70 && number < 80){
+
+        document.getElementById("result_show_english").innerHTML ='english: a'.toUpperCase();
+
+    }else if(number >=80 && number < 100){
+
+        document.getElementById("result_show_english").innerHTML ='english: a+'.toUpperCase();
+    }else{
+
+        document.getElementById("result_show_english").innerHTML ='you are fail in english'.toUpperCase();
+
+    }
+}
+
+function gread_math(number){
+
+    if(number <0 || number > 100){
+
+        document.getElementById("result_show_math").innerHTML ='input valide number'.toUpperCase();
+        alert('Please Check Your math subject Number')
+     
+    }else if(number >=33 && number < 40){
+
+        document.getElementById("result_show_math").innerHTML ='math: d'.toUpperCase();
+
+    }else if(number >=40 && number < 50){
+
+        document.getElementById("result_show_math").innerHTML ='math: c'.toUpperCase();
+
+    }else if(number >=50 && number < 60){
+
+        document.getElementById("result_show_math").innerHTML ='math: b'.toUpperCase();
+
+    }else if(number >=60 && number < 70){
+
+        document.getElementById("result_show_math").innerHTML ='math: a-'.toUpperCase();
+
+    }else if(number >=70 && number < 80){
+
+        document.getElementById("result_show_math").innerHTML ='math: a'.toUpperCase();
+
+    }else if(number >=80 && number < 100){
+
+        document.getElementById("result_show_math").innerHTML ='math: a+'.toUpperCase();
+    }else{
+
+        document.getElementById("result_show_math").innerHTML ='you are fail in math'.toUpperCase();
+
+    }
+}
+
+function gread_arbi(number){
+
+    if(number <0 || number > 100){
+
+        document.getElementById("result_show_arbi").innerHTML ='input valide number'.toUpperCase();
+        alert('Please Check Your arbi subject Number')
+     
+    }else if(number >=33 && number < 40){
+
+        document.getElementById("result_show_arbi").innerHTML ='arbi: d'.toUpperCase();
+
+    }else if(number >=40 && number < 50){
+
+        document.getElementById("result_show_arbi").innerHTML ='arbi: c'.toUpperCase();
+
+    }else if(number >=50 && number < 60){
+
+        document.getElementById("result_show_arbi").innerHTML ='arbi: b'.toUpperCase();
+
+    }else if(number >=60 && number < 70){
+
+        document.getElementById("result_show_arbi").innerHTML ='arbi: a-'.toUpperCase();
+
+    }else if(number >=70 && number < 80){
+
+        document.getElementById("result_show_arbi").innerHTML ='arbi: a'.toUpperCase();
+
+    }else if(number >=80 && number < 100){
+
+        document.getElementById("result_show_arbi").innerHTML ='arbi: a+'.toUpperCase();
+    }else{
+
+        document.getElementById("result_show_arbi").innerHTML ='you are fail in arbi'.toUpperCase();
+
+    }
+}
+
+function gread_quran(number){
+
+    if(number <0 || number > 100){
+
+        document.getElementById("result_show_quran").innerHTML ='input valide number'.toUpperCase();
+        alert('Please Check Your quran subject Number')
+     
+    }else if(number >=33 && number < 40){
+
+        document.getElementById("result_show_quran").innerHTML ='quran: d'.toUpperCase();
+
+    }else if(number >=40 && number < 50){
+
+        document.getElementById("result_show_quran").innerHTML ='quran: c'.toUpperCase();
+
+    }else if(number >=50 && number < 60){
+
+        document.getElementById("result_show_quran").innerHTML ='quran: b'.toUpperCase();
+
+    }else if(number >=60 && number < 70){
+
+        document.getElementById("result_show_quran").innerHTML ='quran: a-'.toUpperCase();
+
+    }else if(number >=70 && number < 80){
+
+        document.getElementById("result_show_quran").innerHTML ='quran: a'.toUpperCase();
+
+    }else if(number >=80 && number < 100){
+
+        document.getElementById("result_show_quran").innerHTML ='quran: a+'.toUpperCase();
+    }else{
+
+        document.getElementById("result_show_quran").innerHTML ='you are fail in quran'.toUpperCase();
+
+    }
+}
+
+function gread_hadis(number){
+
+    if(number <0 || number > 100){
+
+        document.getElementById("result_show_hadis").innerHTML ='input valide number'.toUpperCase();
+        alert('Please Check Your hadis subject Number')
+     
+    }else if(number >=33 && number < 40){
+
+        document.getElementById("result_show_hadis").innerHTML ='hadis: d'.toUpperCase();
+
+    }else if(number >=40 && number < 50){
+
+        document.getElementById("result_show_hadis").innerHTML ='hadis: c'.toUpperCase();
+
+    }else if(number >=50 && number < 60){
+
+        document.getElementById("result_show_hadis").innerHTML ='hadis: b'.toUpperCase();
+
+    }else if(number >=60 && number < 70){
+
+        document.getElementById("result_show_hadis").innerHTML ='hadis: a-'.toUpperCase();
+
+    }else if(number >=70 && number < 80){
+
+        document.getElementById("result_show_hadis").innerHTML ='hadis: a'.toUpperCase();
+
+    }else if(number >=80 && number < 100){
+
+        document.getElementById("result_show_hadis").innerHTML ='hadis: a+'.toUpperCase();
+    }else{
+
+        document.getElementById("result_show_hadis").innerHTML ='you are fail in hadis'.toUpperCase();
+
+    }
+}
